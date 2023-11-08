98ed5fa084bcf3a4e1e2a5a97d462ccfe879b1c0
skyrizer <wafir2002@gmail.com>
2023-11-08 18:21:26 +0800

    add image picker

--- lib/pages/post/new_post.dart
+++ lib/pages/post/new_post.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:CatViP/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class NewPost extends StatefulWidget {
   const NewPost({super.key});
@@ -12,51 +15,192 @@ class NewPost extends StatefulWidget {
 
 class _NewPostState extends State<NewPost> {
 
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
+  
+  Future getImage(ImageSource source) async{
+    final pickedFile = await _picker.pickImage(source: source);
+    
+    if(pickedFile != null){
+      image = File(pickedFile.path);
+      setState(() {
+        
+      });
+    }else{
+      print("No Image Selected");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
         appBar: AppBar(
           title: Text("Add post"),
         ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _caption(),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  children: <Widget>[
+                    insertImage(context),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    caption(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-
-
-
-  Widget _caption(){
-    return TextFormField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.teal,
-
+  Widget bottomSheet(BuildContext context){
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text("Insert Image Here.",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton.icon(
+                icon: Icon(Icons.camera),
+                onPressed: () {
+                  getImage(ImageSource.camera);
+                },
+                label: Text("Camera"),
+              ),
+              TextButton.icon(
+                icon: Icon(Icons.image),
+                onPressed: () {
+                  getImage(ImageSource.gallery);
+                },
+                label: Text("Gallery"),
+              ),
+            ],
           )
+        ],
+      ),
+    );
+  }
+
+  /*
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(
+      source: source,
+    );
+    setState((){
+      _imageFile = pickedFile as PickedFile;
+    });
+  }*/
+
+  Widget insertImage(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: ((builder) => bottomSheet(context)),
+              );
+            },
+            child: Container(
+              child: image == null
+                  ? Center(child: Text('Pick Image'))
+                  : Container(
+                child: Center(
+                  child: Image.file(
+                    //File(image!.path).absolute,
+                    File(image!.path),
+                    height: 300,
+                    width: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20.0,
+            right: 20.0,
+            child: InkWell(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: ((builder) => bottomSheet(context)),
+                );
+              },
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.teal,
+                size: 28.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget caption(){
+    return Container(
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            )
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )
+          ),
+          prefixIcon: Icon(
+            Icons.closed_caption,
+            color: Colors.green,
+          ),
+          labelText: "Caption",
+            helperText: "",
+          hintText: "Write a caption.",
         ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )
-        ),
-        prefixIcon: Icon(
-          Icons.closed_caption,
-          color: Colors.green,
-        ),
-        labelText: "Caption",
-          helperText: "",
-        hintText: "Write a caption.",
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,3 +1,4 @@
+
 import 'package:CatViP/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
