0a0682a58a15da162039a01eec41c75c5521eff4
YUNG HUEY TONG <yunghuey2002@gmail.com>
2023-11-23 13:32:27 +0800

    edit profile new page

---
+++ lib/pages/user/editprofile_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add new cat', style: Theme.of(context).textTheme.bodyLarge),
+        backgroundColor: HexColor("#ecd9c9"),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+      ),
+      body: Text("hello edit user"),
+    );
+  }
+}
