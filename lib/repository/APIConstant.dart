2e1ceaa9f24df593990d3cd1bddc19b6ebeaa64e
YUNG HUEY TONG <yunghuey2002@gmail.com>
2024-01-09 20:15:05 +0800

    change yh ipaddress

--- lib/repository/APIConstant.dart
+++ lib/repository/APIConstant.dart
@@ -1,6 +1,6 @@
 class APIConstant {
   // yunghuey
-  static const String ipaddress = "http://10.131.78.121:7015/";
+  static const String ipaddress = "http://10.131.77.249:7015/";
   // wafir
   // static const String ipaddress = "http://10.131.151.180:45455/";
   static const String URL = "${ipaddress}api/";
